c728999264b4f787c3e111a16ab15a14d439d433
Maarten Marsman <m.marsman@uva.nl>
2025-09-23 20:54:35 +0200

    Fixes testthat

--- tests/testthat/test-bgm.R
+++ tests/testthat/test-bgm.R
@@ -1,5 +1,5 @@
 test_that("Posterior means correlate with sufficient precision statistics", {
-  fit = bgm(Wenchuan, edge_selection = FALSE, iter = 1e2, seed = 1234, chains = 1)
+  fit = bgm(Wenchuan, edge_selection = FALSE, iter = 100, warmup = 1000, seed = 1234, chains = 1)
   x = Wenchuan
   x = na.omit(x)
   alt = -solve(t(x)%*%x)
@@ -24,35 +24,21 @@ test_that("bgmCompare is reproducible", {
   data("Wenchuan", package = "bgms")
   x <- Wenchuan[1:50, 1:5]
   y <- Wenchuan[1:50, c(1:4, 6)]
-  fit1 <- bgmCompare2(x = x, y = y, iter = 100, warmup = 1000, cores = no_cores, seed = 1234)
-  fit2 <- bgmCompare2(x = x, y = y, iter = 100, warmup = 1000, cores = no_cores, seed = 1234)
+  fit1 <- bgmCompare(x = x, y = y, iter = 100, warmup = 1000, cores = no_cores, seed = 1234)
+  fit2 <- bgmCompare(x = x, y = y, iter = 100, warmup = 1000, cores = no_cores, seed = 1234)
 
-  combine_chains <- function(lst) {
-    # without abind
-    element_names <- names(lst[[1]])
-
-    out <- lapply(element_names, function(nm) {
-      arrays <- lapply(lst, function(chain) chain[[nm]])
-      dims <- dim(arrays[[1]])
-      if (is.null(dims)) {
-        # handle scalar/vector case (e.g. chain_id)
-        return(unlist(arrays))
-      } else {
-        # bind along a new last dimension
-        arr <- array(
-          unlist(arrays),
-          dim = c(dims, length(arrays))
-        )
-        return(arr)
-      }
-    })
-
-    names(out) <- element_names
-    out
+  combine_chains <- function(fit) {
+    pairs <- fit$raw_samples$pairwise
+    pair  <- do.call(rbind, pairs)
+    mains <- fit$raw_samples$main
+    main  <- do.call(rbind, mains)
+    inds <- fit$raw_samples$indicator
+    ind  <- do.call(rbind, inds)
+    return(cbind(main, pair, ind))
   }
 
-  combined1 <- combine_chains(samples1)
-  combined2 <- combine_chains(samples2)
+  combined1 <- combine_chains(fit1)
+  combined2 <- combine_chains(fit2)
 
   testthat::expect_equal(combined1, combined2)
 })
